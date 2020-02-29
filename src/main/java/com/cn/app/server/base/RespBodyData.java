efeab33ff791f973a3cb80d52d5a6885e01b516f
skysgit <393740104@qq.com>
2020-02-29 16:55:06 +0800

    ss

--- src/main/java/com/cn/app/server/base/RespBodyData.java
+++ src/main/java/com/cn/app/server/base/RespBodyData.java
@@ -1,13 +1,10 @@
 package com.cn.app.server.base;
 
-import com.cn.app.server.model.Users;
-
 /**
  * 	存放实体类
- * @author Admin CX
+ * @author chenxiang
  *
  */
 public class RespBodyData{
  
-	public Users users;
 }
